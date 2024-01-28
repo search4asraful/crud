8a20b739ee2ebd0a799291e1dc48dd6ef0286a18
search4asraful <asrafulvau009@gmail.com>
2024-01-28 19:50:54 +0600

    change title

--- update.php
+++ update.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Crud Update</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container col-md-12 mt-3">
+    <div class="container col-md-12 mt-5">
       <div class="col-md-3"></div>
       <div class="card mx-auto col-md-6">
           <div class="card-header">
@@ -18,17 +18,17 @@
           <form action="#" method="POST">
           <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="name" placeholder="Enter full name" required>
+            <input type="text" name="name" class="form-control" id="name" placeholder="Enter full name" required>
           </div>
           <div class="mb-3">
             <label for="phone" class="form-label">Phone</label>
-            <input type="number" class="form-control" id="phone" placeholder="Enter phone number" required>
+            <input type="tel" name="phone" class="form-control" id="phone" placeholder="Enter phone number" required>
           </div>
           <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email address" required>
+            <input type="email" name="email" class="form-control" id="email" placeholder="Enter email address" required>
           </div>
-          <button type="submit" class="btn btn-warning">Update info</button>
+          <button type="submit" name="submit" class="btn btn-warning">Update info</button>
         </form>
           </div>
         </div>
