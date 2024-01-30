0c2c3d7201462e3cc8ae42da9c3b6e2d8839af4e
search4asraful <asrafulvau009@gmail.com>
2024-01-30 19:58:18 +0600

    remove: Bootstrap js cdn

--- create.php
+++ create.php
@@ -1,15 +1,6 @@
 <?php
   include 'config.php';
 
-  $errors = [];
-  function validateInput($value, $errorMsg) {
-    global $errors;
-    if(empty($value)) {
-      $errors[] = $errorMsg;
-    }else {
-      return $value;
-    }
-  }
   if (isset($_POST['submit'])) {
     $name = validateInput($_POST['name'], "Name field is required");
     $phone = validateInput($_POST['phone'], "Phone field is required");
@@ -76,7 +67,5 @@
         </div>
       <div class="col-md-3"></div>
     </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
